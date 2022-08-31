a180149aec60310c1333148eed8e453e5fbea60b
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-08-31 10:57:29 -0400

    building model functions for users

--- api/auth/auth-model.js
+++ api/auth/auth-model.js
@@ -1,1 +1,19 @@
 const db = require('../../data/dbConfig');
+
+module.exports = {
+    findBy,
+    findById,
+    create
+}
+
+function findBy(filter) {
+return db('users').where(filter);
+//.first()
+}
+
+
+
+async function create(user) {
+const [ id ] = await db('users').insert(user)
+return findById(id)
+}
